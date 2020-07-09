cf5027c790b9d99d797e022a24fa8ff08b54c6a5
MARSHAL327 <sanya.shvedenko@mail.ru>
2020-07-09 18:27:42 +0300

    +admin +student +human

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+struct Human
+{
+    public string fio;
+    public int age;
+    public char gender;
+}
+
+class Student
+{
+
+}
+
+class Admin
+{
+    Human H;
+    List<string> allNews = new List<string>();
+    List<Student> allStudents;
+
+    public Admin(string _fio, int _age, char _gender)
+    {
+        H.fio = _fio;
+        H.age = _age;
+        H.gender = _gender;
+    }
+
+    public void AddNews(string newNews)
+    {
+        allNews.Add(newNews);
+    }
+
+    public void ShowStudentList(Student students)
+    {
+
+    }
+
+    public void AddStudent(string _fio, int _age, char _gender)
+    {
+
+    }
+
+    public void GetNews()
+    {
+        foreach (string news in allNews)
+        {
+            Console.WriteLine(news);
+        }
+    }
+}
+
+namespace VUZ_System
+{
+    class Program
+    {
+        static void Main()
+        {
+            Admin A = new Admin("Alexandr", 18, 'm');
+            A.AddNews("myNews");
+            A.AddNews("newNews");
+            A.AddNews("hello");
+            A.GetNews();
+        }
+    }
+}
